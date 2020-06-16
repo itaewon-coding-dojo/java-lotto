199428bb79078472c5beed7aace19a0dc9a28e91
gyim1345 <gyim1345@gmail.com>
2020-06-16 17:40:24 +0900

    Add Deciferer to extract elements
    Add Deciferer Class and extractElements Method

--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -1,6 +1,6 @@
 public class CalculatorApp {
     public static void main(String[] args) {
         String input = InputView.getInputs();
-
+        String[] inputs = Deciferer.extractElements(input);
     }
 }
---
+++ src/main/java/Deciferer.java
@@ -0,0 +1,6 @@
+public class Deciferer {
+
+    public static String[] extractElements (String inputs) {
+        return new String[] {"a"};
+    }
+}
