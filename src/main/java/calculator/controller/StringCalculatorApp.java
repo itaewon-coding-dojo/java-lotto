a28e91933d54040daf00ef07431d879f31a9efda
woomin <dal96k@gmail.com>
2020-06-16 18:30:37 +0900

    Make StringCalculationApp.java

--- src/main/java/calculator/controller/StringCalculatorApp.java
+++ src/main/java/calculator/controller/StringCalculatorApp.java
@@ -1,7 +1,18 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.model.StringReader;
+import calculator.view.Input;
+
 public class StringCalculatorApp {
     public static void main(String[] args) {
+        System.out.println("수식을 입력해주세요");
+        String userInput = Input.getUserInput();
 
+        StringReader stringReader = new StringReader();
+        int[] arr = stringReader.read(userInput);
+
+        Calculator calculator = new Calculator(arr);
+        System.out.println(calculator.executeAddition());
     }
 }
