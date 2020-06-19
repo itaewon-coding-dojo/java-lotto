bd9d719a67998adc81f8ab21fe2a71ef5574da73
woomin <dal96k@gmail.com>
2020-06-19 15:33:06 +0900

    Add result view

--- src/main/java/lotto/controller/LottoApplication.java
+++ src/main/java/lotto/controller/LottoApplication.java
@@ -1,8 +1,11 @@
 package lotto.controller;
 
+import lotto.domain.LottoChecker;
 import lotto.domain.LottoMachine;
 import lotto.view.Input;
 import lotto.view.Output;
+
+import java.util.Map;
 
 public class LottoApplication {
     public static void main(String[] args) {
@@ -13,7 +16,10 @@ public class LottoApplication {
 
         Output.showLottoTickets(lottoMachine.getTickets());
 
-        String winning = Input.getWinningNumbers();
+        String winningNumbers = Input.getWinningNumbers();
+        LottoChecker lottoChecker = LottoChecker.newChecker(winningNumbers);
+        Map<Integer, Integer> counter = lottoChecker.checkAllTickets(lottoMachine.getTickets());
 
+        Output.showResult(counter, money);
     }
 }
