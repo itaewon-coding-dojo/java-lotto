a52d76445fc3c77879f7603327bad8f74c05bac2
woomin <dal96k@gmail.com>
2020-06-19 14:30:06 +0900

    Add getUserInputMoney() in Input

---
+++ src/main/java/lotto/controller/LottoApplication.java
@@ -0,0 +1,10 @@
+package lotto.controller;
+
+import lotto.view.Input;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+        int money = Input.getUserInputMoney();
+
+    }
+}
---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class Input {
+    public static int getUserInputMoney() {
+        System.out.println("로또 구입 금액을 입력해주세요");
+        Scanner scanner = new Scanner(System.in);
+
+        int money = scanner.nextInt();
+        System.out.printf("%d개를 구매하셨습니다.", money / 1000);
+
+        return money;
+    }
+}
